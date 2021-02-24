aa602e5bedf65d425a722be2ce16ce6392d0c19c
gulenzek <73951604+gulenzek@users.noreply.github.com>
2021-02-24 09:56:09 +0300

    Update soru9_ascii_tr.py

--- sinav_sorulari/soru9_ascii_tr.py
+++ sinav_sorulari/soru9_ascii_tr.py
@@ -1,29 +1,37 @@
 # -*- --coding:utf-8 -*-
 # @author: gulenzek
 """
-
+ç - c
+Ç - C
+ğ - g
+Ğ - Ğ
+ı - i
+İ - I
+ö - o
+Ö - O
+ş - s
+Ş - S
+ü - u
+Ü - U
 """
 def ascii_tr(text):
-    case = {}
-    try:
-        case = {
-            'ğ': 'g',
-            'ü': 'u',
-            'ş': 's',
-            'ı': 'i',
-            'ö': 'o',
-            'Ğ': 'G',
-            'Ü': 'U',
-            'Ş': 'S',
-            'İ': 'I',
-            'Ö': 'O'
-        }
-        print("bura")
-    except KeyError:
-        
+    text1 = text.replace("ç", "c")
+    text2 = text1.replace("Ç", "C")
+    text3 = text2.replace("ğ", "g")
+    text4 = text3.replace("Ğ", "G")
+    text5 = text4.replace("ı", "i")
+    text6 = text5.replace("İ", "I")
+    text7 = text6.replace("ö", "o")
+    text8 = text7.replace("Ö", "O")
+    text9 = text8.replace("ş", "s")
+    text10 = text9.replace("Ş", "S")
+    text11 = text10.replace("ü", "u")
+    text12 = text11.replace("Ü", "U")
+    return text12
 
 def main():
-
+    meraba = ascii_tr("şanlıürfİ")
+    print(meraba)
 
 
 if __name__ == "__main__":
